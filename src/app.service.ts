6401c3bad8ad45d0802eb43eb9d8a7599404127d
Ted Moens <ted.moens@gmail.com>
2021-04-18 06:52:22 -0700

    make a more usefull message for the base URL

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import { Observable, of } from "rxjs";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AppService {
-  
-  constructor() {
+
+  constructor(
+    private configService: ConfigService,
+  ) {
   }
+
   getHello(): string {
-    return 'Hello World!';
+    const message: string[] = [];
+    message.push('<p>This API fills a need that the APIs at ZFIN.org do not presently fulfill. ' +
+      'Given a zebrafish mutation or transgene allele name, it will supply the ZFIN Id of that mutation or allele. ' +
+      'The supported calls are:');
+    message.push(`${this.configService.get('MY_URL')}/mutation/allele/your_mutation_allele_here`)
+    message.push(`${this.configService.get('MY_URL')}/transgene/allele/your_transgene_allele_here`)
+    message.push('In future there may be other functions such as mutation/search/some_search_critieria_here')
+
+    return message.join('</p><p>');
   }
-  
 }
