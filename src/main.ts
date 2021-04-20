268de75f8bfd4a6b462a2bdc854033159b579ac1
Ted Moens <ted.moens@gmail.com>
2021-04-19 17:04:25 -0700

    Add cors support to server

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
-import { ConfigService } from "@nestjs/config";
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  app.enableCors();
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('APPLICATION_PORT'));
